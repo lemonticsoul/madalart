e1fa797d2f9f1aa2493a5d26c025a34f6a6f7c19
lemonticsoul <127959482+lemonticsoul@users.noreply.github.com>
2024-02-19 08:47:55 +0900

    Add files via upload

--- madalart/madalart/src/main/java/com/example/madalart/service/MadalartService.java
+++ madalart/madalart/src/main/java/com/example/madalart/service/MadalartService.java
@@ -26,19 +26,19 @@ public class MadalartService {
     }
 
     public MainTopicDto saveMainTopics(MainTopicDto mainTopicDto) {
-        // DTO를 엔티티로 변환
+
         MainTopic mainTopic = new MainTopic();
         mainTopic.setTitle(mainTopicDto.getTitle());
-        // mainTopic에 필요한 다른 필드도 여기에서 설정
 
-        // 엔티티 저장
+
+
         mainTopic = mainRepository.save(mainTopic);
 
-        // 저장된 엔티티를 DTO로 변환하여 반환
+
         MainTopicDto savedDto = new MainTopicDto();
         savedDto.setId(mainTopic.getId());
         savedDto.setTitle(mainTopic.getTitle());
-        // savedDto에 필요한 다른 필드도 여기에서 설정
+
 
         return savedDto;
     }
@@ -46,7 +46,7 @@ public class MadalartService {
     public List<MainTopicDto> getAllMainTopics(){
         List<MainTopic> mainTopics = mainRepository.findAll();
 
-        // 엔티티를 DTO로 변환
+
         List<MainTopicDto> mainTopicDtos = mainTopics.stream()
                 .map(this:: convertEntityToDto)
                 .collect(Collectors.toList());
@@ -58,7 +58,7 @@ public class MadalartService {
         MainTopicDto mainTopicDto = new MainTopicDto();
         mainTopicDto.setId(mainTopic.getId());
         mainTopicDto.setTitle(mainTopic.getTitle());
-        // 여기에 필요한 다른 필드들을 설정
+
         return mainTopicDto;
     }
 
@@ -68,17 +68,17 @@ public class MadalartService {
         if(mainTopicOpt.isPresent()) {
             MainTopic mainTopic = mainTopicOpt.get();
 
-            // 중앙 주제에 연관된 세부 정보 조회
+
             List<Detail> details = detailRepository.findByMainTopicId(mainTopic.getId());
 
-            // 엔티티를 DTO로 변환
+
             MainTopicDto mainTopicDto = convertEntityToDto(mainTopic);
 
-            // 세부 정보 DTO로 변환하여 MainTopicDto에 추가
+
             List<DetailDto> detailDtos = details.stream()
-                    .map(this::convertDetailEntityToDto) // 세부 정보 엔티티를 DTO로 변환하는 메서드 필요
+                    .map(this::convertDetailEntityToDto)
                     .collect(Collectors.toList());
-            mainTopicDto.setDetails(detailDtos); // MainTopicDto에 세부 정보 설정
+            mainTopicDto.setDetails(detailDtos);
 
             return mainTopicDto;
 
@@ -104,10 +104,10 @@ public class MadalartService {
         detail.setContent(detailDto.getContent());
         detail.setMainTopicId(detailDto.getMainTopicId());
 
-        // 데이터베이스에 저장
+
         detail = detailRepository.save(detail);
 
-        // 저장된 엔티티를 DTO로 변환하여 반환
+        
         DetailDto savedDetailDto = new DetailDto();
         savedDetailDto.setId(detail.getId());
         savedDetailDto.setContent(detail.getcontent());
